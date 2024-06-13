f01b5ac7a47b7469df5223b64b71ace0a43e9dd3
ChangerLi <akashi2ny@gmail.com>
2024-06-13 17:51:27 -0400

    formatting

--- UU Decoder/uu_decoder.py
+++ UU Decoder/uu_decoder.py
@@ -10,8 +10,10 @@ for i in range(8):
         uuencode_table[character] = i * 8 + j
 
 # Using ‘ to replace space is not universal.
-# Some codes may use ` instead. 
-uuencode_table['`'] = 0
+# Some codes may use ` instead.
+uuencode_table["`"] = 0
+
+
 def uudecoder(encoded_data: str):
     decoded_string = ""
     idx = 0
@@ -19,17 +21,16 @@ def uudecoder(encoded_data: str):
         num_bytes_in_line = uuencode_table[encoded_data[idx]]
         idx += 1
         while num_bytes_in_line > 0 and idx < len(encoded_data):
-            binaries = ''
+            binaries = ""
             for i in range(4):
                 temp = bin(uuencode_table[encoded_data[idx]])[2:].zfill(6)
                 binaries += temp
                 idx += 1
-                
+
             # Break into 3 bytes and for each byte, convert it to a character
             # and append to decoded string
             for i in range(3):
-                decoded_string += chr(int(binaries[i*8: i*8 + 8], 2))
+                decoded_string += chr(int(binaries[i * 8 : i * 8 + 8], 2))
             num_bytes_in_line -= 3
 
     return decoded_string
-        
